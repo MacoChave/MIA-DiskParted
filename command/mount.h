37b9519f8bd3a370cbd5f460ad9381d04c0c85b5
Maco Chave <catalantt@gmail.com>
2019-09-19 13:33:53 -0600

    Cambiar sd a vd para montaje. Mostrar en consola externa

--- command/mount.h
+++ command/mount.h
@@ -137,12 +137,12 @@ void showMountedPartition()
     {
         if (disks_mount[i].letter == '0') continue;
 
-        printf(ANSI_COLOR_BLUE "sd%c\n" ANSI_COLOR_RESET, disks_mount[i].letter);
+        printf(ANSI_COLOR_BLUE "vd%c\n" ANSI_COLOR_RESET, disks_mount[i].letter);
         for (int j = 0; j < 20; j++)
         {
             if (disks_mount[i].parts_mount[j].mount_id == 0) continue;
             
-            printf(ANSI_COLOR_BLUE "\tsd%c%d\n" ANSI_COLOR_RESET, disks_mount[i].letter, disks_mount[i].parts_mount[j].mount_id);
+            printf(ANSI_COLOR_BLUE "\tvd%c%d\n" ANSI_COLOR_RESET, disks_mount[i].letter, disks_mount[i].parts_mount[j].mount_id);
         }
     }    
 }
